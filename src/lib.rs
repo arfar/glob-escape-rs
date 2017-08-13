dc9741b01548f8f850bde579e0ba731a5afa9e64
Arthur Roberts <arfa.r@live.com>
2017-08-13 03:25:31 +0000

    Changed escape to take &str

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub fn escape(unescaped: String) -> String {
+pub fn escape(unescaped: &str) -> String {
     let mut escaped = String::with_capacity(unescaped.len());
 
     for c in unescaped.chars() {
@@ -20,127 +20,106 @@ mod tests {
 
     #[test]
     fn no_escaping_1() {
-        let s1 = String::from("test");
-        assert_eq!("test", escape(s1));
+        assert_eq!("test", escape("test"));
     }
 
     #[test]
     fn no_escaping_2() {
-        let s1 = String::from("test]");
-        assert_eq!("test]", escape(s1));
+        assert_eq!("test]", escape("test]"));
     }
 
     #[test]
     fn bracket_1() {
-        let s1 = String::from("[test");
-        assert_eq!("\\[test", escape(s1));
+        assert_eq!("\\[test", escape("[test"));
     }
 
     #[test]
     fn bracket_2() {
-        let s1 = String::from("[test]");
-        assert_eq!("\\[test]", escape(s1));
+        assert_eq!("\\[test]", escape("[test]"));
     }
 
     #[test]
     fn bracket_3() {
-        let s1 = String::from("test[]");
-        assert_eq!("test\\[]", escape(s1));
+        assert_eq!("test\\[]", escape("test[]"));
     }
 
     #[test]
     fn bracket_4() {
-        let s1 = String::from("test[");
-        assert_eq!("test\\[", escape(s1));
+        assert_eq!("test\\[", escape("test["));
     }
 
     #[test]
     fn bracket_5() {
-        let s1 = String::from("test[[");
-        assert_eq!("test\\[\\[", escape(s1));
+        assert_eq!("test\\[\\[", escape("test[["));
     }
 
     #[test]
     fn bracket_6() {
-        let s1 = String::from("[[test[[");
-        assert_eq!("\\[\\[test\\[\\[", escape(s1));
+        assert_eq!("\\[\\[test\\[\\[", escape("[[test[["));
     }
 
     #[test]
     fn asterisk_1() {
-        let s1 = String::from("*test");
-        assert_eq!("\\*test", escape(s1));
+        assert_eq!("\\*test", escape("*test"));
     }
 
     #[test]
     fn asterisk_2() {
-        let s1 = String::from("*test]");
-        assert_eq!("\\*test]", escape(s1));
+        assert_eq!("\\*test]", escape("*test]"));
     }
 
     #[test]
     fn asterisk_3() {
-        let s1 = String::from("test*]");
-        assert_eq!("test\\*]", escape(s1));
+        assert_eq!("test\\*]", escape("test*]"));
     }
 
     #[test]
     fn asterisk_4() {
-        let s1 = String::from("test*");
-        assert_eq!("test\\*", escape(s1));
+        assert_eq!("test\\*", escape("test*"));
     }
 
     #[test]
     fn asterisk_5() {
-        let s1 = String::from("test**");
-        assert_eq!("test\\*\\*", escape(s1));
+        assert_eq!("test\\*\\*", escape("test**"));
     }
 
     #[test]
     fn asterisk_6() {
-        let s1 = String::from("**test**");
-        assert_eq!("\\*\\*test\\*\\*", escape(s1));
+        assert_eq!("\\*\\*test\\*\\*", escape("**test**"));
     }
 
     #[test]
     fn qmark_1() {
-        let s1 = String::from("?test");
-        assert_eq!("\\?test", escape(s1));
+        assert_eq!("\\?test", escape("?test"));
     }
 
     #[test]
     fn qmark_2() {
-        let s1 = String::from("?test]");
-        assert_eq!("\\?test]", escape(s1));
+        assert_eq!("\\?test]", escape("?test]"));
     }
 
     #[test]
     fn qmark_3() {
-        let s1 = String::from("test?]");
-        assert_eq!("test\\?]", escape(s1));
+        assert_eq!("test\\?]", escape("test?]"));
     }
 
     #[test]
     fn qmark_4() {
-        let s1 = String::from("test?");
-        assert_eq!("test\\?", escape(s1));
+        assert_eq!("test\\?", escape("test?"));
     }
 
     #[test]
     fn qmark_5() {
-        let s1 = String::from("test??");
-        assert_eq!("test\\?\\?", escape(s1));
+        assert_eq!("test\\?\\?", escape("test??"));
     }
 
     #[test]
     fn qmark_6() {
-        let s1 = String::from("??test??");
-        assert_eq!("\\?\\?test\\?\\?", escape(s1));
+        assert_eq!("\\?\\?test\\?\\?", escape("??test??"));
     }
 
     #[test]
     fn mixed_1() {
-        let s1 = String::from("[?]test[*]");
-        assert_eq!("\\[\\?]test\\[\\*]", escape(s1));
+        assert_eq!("\\[\\?]test\\[\\*]", escape("[?]test[*]"));
     }
 }
