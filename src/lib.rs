a7985231ec37012d15958641fe7e20a03e3b90b7
Arthur Roberts <arfa.r@live.com>
2017-08-13 20:37:17 +1200

    Changed how pattern characters are handled
    Escaping using the glob pattern system rather than trying to escape the
    characters.

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,10 @@ pub fn escape(unescaped: &str) -> String {
 
     for c in unescaped.chars() {
         match c {
-            '['|'?'|'*' => {
-                escaped.push('\\');
+            '['|']'|'?'|'*' => {
+                escaped.push('[');
                 escaped.push(c);
+                escaped.push(']');
             },
             _ => escaped.push(c),
         }
@@ -25,101 +26,101 @@ mod tests {
 
     #[test]
     fn no_escaping_2() {
-        assert_eq!("test]", escape("test]"));
+        assert_eq!("test[]]", escape("test]"));
     }
 
     #[test]
     fn bracket_1() {
-        assert_eq!("\\[test", escape("[test"));
+        assert_eq!("[[]test", escape("[test"));
     }
 
     #[test]
     fn bracket_2() {
-        assert_eq!("\\[test]", escape("[test]"));
+        assert_eq!("[[]test[]]", escape("[test]"));
     }
 
     #[test]
     fn bracket_3() {
-        assert_eq!("test\\[]", escape("test[]"));
+        assert_eq!("test[[][]]", escape("test[]"));
     }
 
     #[test]
     fn bracket_4() {
-        assert_eq!("test\\[", escape("test["));
+        assert_eq!("test[[]", escape("test["));
     }
 
     #[test]
     fn bracket_5() {
-        assert_eq!("test\\[\\[", escape("test[["));
+        assert_eq!("test[[][[]", escape("test[["));
     }
 
     #[test]
     fn bracket_6() {
-        assert_eq!("\\[\\[test\\[\\[", escape("[[test[["));
+        assert_eq!("[[][[]test[[][[]", escape("[[test[["));
     }
 
     #[test]
     fn asterisk_1() {
-        assert_eq!("\\*test", escape("*test"));
+        assert_eq!("[*]test", escape("*test"));
     }
 
     #[test]
     fn asterisk_2() {
-        assert_eq!("\\*test]", escape("*test]"));
+        assert_eq!("[*]test[]]", escape("*test]"));
     }
 
     #[test]
     fn asterisk_3() {
-        assert_eq!("test\\*]", escape("test*]"));
+        assert_eq!("test[*][]]", escape("test*]"));
     }
 
     #[test]
     fn asterisk_4() {
-        assert_eq!("test\\*", escape("test*"));
+        assert_eq!("test[*]", escape("test*"));
     }
 
     #[test]
     fn asterisk_5() {
-        assert_eq!("test\\*\\*", escape("test**"));
+        assert_eq!("test[*][*]", escape("test**"));
     }
 
     #[test]
     fn asterisk_6() {
-        assert_eq!("\\*\\*test\\*\\*", escape("**test**"));
+        assert_eq!("[*][*]test[*][*]", escape("**test**"));
     }
 
     #[test]
     fn qmark_1() {
-        assert_eq!("\\?test", escape("?test"));
+        assert_eq!("[?]test", escape("?test"));
     }
 
     #[test]
     fn qmark_2() {
-        assert_eq!("\\?test]", escape("?test]"));
+        assert_eq!("[?]test[]]", escape("?test]"));
     }
 
     #[test]
     fn qmark_3() {
-        assert_eq!("test\\?]", escape("test?]"));
+        assert_eq!("test[?][]]", escape("test?]"));
     }
 
     #[test]
     fn qmark_4() {
-        assert_eq!("test\\?", escape("test?"));
+        assert_eq!("test[?]", escape("test?"));
     }
 
     #[test]
     fn qmark_5() {
-        assert_eq!("test\\?\\?", escape("test??"));
+        assert_eq!("test[?][?]", escape("test??"));
     }
 
     #[test]
     fn qmark_6() {
-        assert_eq!("\\?\\?test\\?\\?", escape("??test??"));
+        assert_eq!("[?][?]test[?][?]", escape("??test??"));
     }
 
     #[test]
     fn mixed_1() {
-        assert_eq!("\\[\\?]test\\[\\*]", escape("[?]test[*]"));
+        assert_eq!("[[][?][]]test[[][*][]]", escape("[?]test[*]"));
     }
 }
